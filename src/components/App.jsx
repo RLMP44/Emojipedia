ee8b53094600ee84e1100bc92c2601a61d43112a
Rachael Momber <109778611+RLMP44@users.noreply.github.com>
2025-10-28 22:17:54 +0000

    refactor: simplyfy App component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,21 +3,22 @@ import Heading from "./Heading";
 import Entry from "./Entry";
 import emojipedia from "../emojipedia";
 
+function createEntry(entry) {
+  return (
+    <Entry
+      key={entry.id}
+      emoji={entry.emoji}
+      title={entry.name}
+      detail={entry.meaning}
+    />
+  );
+}
+
 function App() {
   return (
     <div>
       <Heading />
-      <div className="dictionary">
-        {emojipedia.map((entry) => {
-          return (
-            <Entry
-              emoji={entry.emoji}
-              title={entry.name}
-              detail={entry.meaning}
-            />
-          );
-        })}
-      </div>
+      <dl className="dictionary">{emojipedia.map(createEntry)}</dl>
     </div>
   );
 }
